08899d8f323d1aea679c2e30c81066e6fb9dd16f
wang jie <wangjie@wangmatoAir.lan>
2016-08-15 23:44:15 +0800

    add filter keyword notify

--- js/background.js
+++ js/background.js
@@ -1,8 +1,10 @@
 "use strict";
 
+
+
 var Chrome = {
     // 通知弹窗
-    createNotification:function(title,Obj){
+    createNotification:function(title){
         var options = {
           type: "basic",
           title: '你订阅的房子有新消息啦！',
@@ -10,13 +12,7 @@ var Chrome = {
           iconUrl: "/icons/zufang.png"
         };
         chrome.notifications.create('null',options, function(){
-        });
-        chrome.notifications.onClicked.addListener(function(id){
-            Obj.map(function(item){
-                chrome.tabs.create({ "url": item,"selected":false });
-            });
-            chrome.notifications.clear(id);
-        });
+        });        
     },
     // 获取background页面
     getBackgroundPage:function(){
@@ -41,22 +37,25 @@ var Chrome = {
     }
 };
 
-var keywords = ['九龙山','七号线','14号线'];
+var keywords = ['九龙山','七号线','14号线','6号线','7号线','独卫'];
 var urls     = ['http://api.douban.com/v2/group/beijingzufang/topics'];
-var crt_url  = [];
+var crt_ids  = [];
 var titles = [];
 
 var autoNotice = function(keywords,urls){
 
-    
+    crt_ids  = [];
+    titles = [];    
+
     urls.map(function(url){
         $.get(url,function(data){
             $.each(data.topics,function(i,data){
-                for(var j=0;j<keywords.length;i++){
-                    var string = data.title+data.content;
-                    if(string.indexOf(keywords[j]) > -1){
+                for(var j=0;j<keywords.length;j++){
+                    var string = data.title + data.content;
+                    // 包括关键字 并且发布时间不能大于两天
+                    if(string.indexOf(keywords[j]) > -1 && (new Date(data.updated) - new Date(data.created) < 48*3600000)){
                         titles.push(data.title);
-                        crt_url.push(`http://www.douban.com/group/topic/${data.id}`);
+                        crt_ids.push(data.id);
                         break;
                     }
                 }
@@ -65,20 +64,39 @@ var autoNotice = function(keywords,urls){
     });
 
 
-    // setTimeout(function(){
-        if(crt_url.length){
-            Chrome.createNotification(titles.join('\n'),crt_url);
+
+    setTimeout(function(){
+
+        var local_ids = localStorage.getItem('view_ids') || '';
+        console.log(crt_ids);
+        if(crt_ids.length){
+            for(var k=0;k<crt_ids.length;k++){
+                if(local_ids.indexOf(crt_ids[k]) == -1){
+                    Chrome.createNotification(titles[k]);
+                    break;
+                }
+            }
+            
+            
         }
-        crt_url = [];
-        titles = [];
-        // autoNotice(keywords,urls);
-    // },60000);
+        autoNotice(keywords,urls);
+    },10000);
 
 };
+localStorage.setItem('view_ids','');
 
-// autoNotice(keywords,urls);
+chrome.notifications.onClicked.addListener(function(id){
+    var local_ids = localStorage.getItem('view_ids') || '';
+    crt_ids.map(function(id){
+        if(localStorage.getItem('view_ids').indexOf(id) == -1){
+            chrome.tabs.create({ "url": `http://www.douban.com/group/topic/${id}`,"selected":false });        
+        }
+    });
+    localStorage.setItem('view_ids',local_ids+crt_ids);
+    chrome.notifications.clear(id);
+});
 
-
+autoNotice(keywords,urls);
 
 
 
